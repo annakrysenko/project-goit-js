be9a8f7d4fb2602af5edcacc3ef4cffa86ededc6
VasylTehlivets <vasyltehlivets@gmail.com>
2023-02-02 17:29:21 +0200

    init filmMarkup

---
+++ src/js/cardMovieMarkup.js
@@ -0,0 +1,30 @@
+export function cardMovieMarkup({
+  id,
+  poster_path,
+  title,
+  genre_ids,
+  release_date,
+  vote_average,
+}) {
+  let markup = `
+   <li class="movie-gallery__item" data-id="${id}">
+    <div class="movie-gallery__poster">
+      <img
+        class="movie-gallery__photo"
+        src="${poster_path}"
+        alt="${title}"
+      />
+    </div>
+    <div class="movie-gallery__wrap">
+      <h2 class="movie-gallery__title">${title}</h2>
+      <div class="movie-info">
+        <p class="movie-info__genre">${genre_ids} |</p>
+        <span class="movie-info__slash> | </span>
+        <p class="movie-info__year">${release_date}</p>
+         <span class="movie-info__vote">${vote_average.toFixed(1)}</span>
+      </div>
+    </div>
+  </li>`;
+
+  return markup;
+}
