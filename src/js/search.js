d99e2e2e55ac8a557036589e9f99cb54f26843ae
Anna Krysenko <krysenkoanna@gmail.com>
2023-02-02 14:31:43 +0200

    last change header

--- src/js/search.js
+++ src/js/search.js
@@ -14,12 +14,18 @@ export  async function getQueryValue(e) {
         makeErrorMassage()
         return
     }
-    console.log()
-    // localStorage.setItem('query', q)
+    refs.inputAnswerParEl.innerHTML = ''
+    
+    return searchFilms
+   
 }
 
 function makeErrorMassage() {
     refs.inputAnswerParEl.innerHTML = 'Search result not successful. Enter the correct movie name and'
 
 }
+
+
+// потім
+// localStorage.setItem('query', q)
 
