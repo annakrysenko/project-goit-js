f6c0bc81648c2b1cc2392593a97ca06f64ddc218
Anna Krysenko <krysenkoanna@gmail.com>
2023-02-02 14:09:34 +0200

    вся верстка хедера

--- src/js/search.js
+++ src/js/search.js
@@ -1,15 +1,25 @@
-import { refs } from "./DOM";
-import {getAxiosSearchFilms } from './axios'
+import { getAxiosSearchFilms } from './axios'
+import { refs } from './DOM';
 
 export  async function getQueryValue(e) {
     e.preventDefault()
     const query = e.target.elements.input.value;
 
-    if (query === '') {
-        //
+    if (query.trim() === '') {
+        makeErrorMassage()
         return;
     }
     const searchFilms = await getAxiosSearchFilms(query)
-    console.log(searchFilms)
+    if (searchFilms.total_results === 0) {
+        makeErrorMassage()
+        return
+    }
+    console.log()
     // localStorage.setItem('query', q)
 }
+
+function makeErrorMassage() {
+    refs.inputAnswerParEl.innerHTML = 'Search result not successful. Enter the correct movie name and'
+
+}
+
