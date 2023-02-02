1f7988c12c825f1b1daf6a5ce78c212f54acca15
SoloviovO <soloviov.o.m@gmail.com>
2023-02-02 22:29:56 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { getQueryValue } from './js/search'
-import {refs} from './js/DOM'
+import { getQueryValue } from './js/search';
+import { refs } from './js/DOM';
+import './js/team-modal';
 
-
-// Пошук по інпуту. Зараз просто консолить об'єкт 
+// Пошук по інпуту. Зараз просто консолить об'єкт
 if (refs.formEl) {
-    refs.formEl.addEventListener('submit', (e) => getFilmsFromInput(e))
+  refs.formEl.addEventListener('submit', e => getFilmsFromInput(e));
 }
-
-    
-
-
--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,6 +1,9 @@
 export const refs = {
-    submitBtnSearchEl: document.querySelector('.submit-search-button'),
-    formEl: document.querySelector('.form-search'),
-    mainHomeEl: document.querySelector('#main-home'),
-    inputAnswerParEl: document.querySelector('.input-answer')
-}
+  submitBtnSearchEl: document.querySelector('.submit-search-button'),
+  formEl: document.querySelector('.form-search'),
+  mainHomeEl: document.querySelector('#main-home'),
+  inputAnswerParEl: document.querySelector('.input-answer'),
+  openModalBtn: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector('.js-team-backdrop'),
+};
