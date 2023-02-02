38c244bed3e10095e4aa7215adb299e10429a064
VasylTehlivets <vasyltehlivets@gmail.com>
2023-02-02 23:18:46 +0200

    init markup

--- src/js/libraryMovieMarkup.js
+++ src/js/libraryMovieMarkup.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD:src/js/сardMovieMarkup.js
 export function libraryMovieMarkup({
-=======
-const BASE_URL = 'https://image.tmdb.org/t/p/w500';
-export function cardMovieMarkup(
->>>>>>> main:src/js/cardMovieMarkup.js
   id,
   poster_path,
   title,
   genre_ids,
   release_date,
-<<<<<<< HEAD:src/js/сardMovieMarkup.js
 }) {
   let markup = `
-=======
-  vote_average
-) {
-  const markup = `
->>>>>>> main:src/js/cardMovieMarkup.js
    <li class="movie-gallery__item" data-id="${id}">
     <div class="movie-gallery__poster">
       <img
@@ -31,10 +20,6 @@ export function cardMovieMarkup(
         <p class="movie-info__genre">${genre_ids} |</p>
         <span class="movie-info__slash> | </span>
         <p class="movie-info__year">${release_date}</p>
-<<<<<<< HEAD:src/js/сardMovieMarkup.js
-=======
-         <span class="movie-info__vote">${vote_average}</span>
->>>>>>> main:src/js/cardMovieMarkup.js
       </div>
     </div>
   </li>`;
