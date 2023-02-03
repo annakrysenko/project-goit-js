47605f673908e49b27d53722195933fb20e2b757
Anna Krysenko <krysenkoanna@gmail.com>
2023-02-03 13:18:14 +0200

    кооректне відображення жанрів в картках і року

--- src/js/cardMovieMarkup.js
+++ src/js/cardMovieMarkup.js
@@ -1,5 +1,6 @@
-const BASE_URL = 'https://image.tmdb.org/t/p/w500';
+import { genresAll } from './genres';
 
+const BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 export function cardMovieMarkup(
   id,
@@ -7,8 +8,11 @@ export function cardMovieMarkup(
   original_title,
   genre_ids,
   release_date,
-  vote_average,
+  vote_average
 ) {
+  console.log(genre_ids);
+  let genresNames = genre_ids.map(genre => genresAll[genre]).join(', ');
+  let releaseDate = release_date.slice(0, 4);
   let markup = `
    <li class="movie-gallery__item" data-id="${id}">
     <div class="movie-gallery__poster">
@@ -21,9 +25,9 @@ export function cardMovieMarkup(
     <div class="movie-gallery__wrap">
       <h2 class="movie-gallery__title">${original_title}</h2>
       <div class="movie-info">
-        <p class="movie-info__genre">${genre_ids} |</p>
+        <p class="movie-info__genre">${genresNames} |</p>
         <span class="movie-info__slash> | </span>
-        <p class="movie-info__year"> ${release_date}</p>
+        <p class="movie-info__year"> ${releaseDate}</p>
          <span class="movie-info__vote">IMDb: ${vote_average}</span>
       </div>
     </div>
