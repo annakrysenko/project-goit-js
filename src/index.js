e7cfcf6f5fdac63a2c7f42cc1ddcec379cdc7f6b
Anna Denysova <annavk2650@gmail.com>
2023-02-03 22:20:09 +0200

    зміна теми

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { getPopularFilms } from './js/get-popular-films';
 
 import { onScroll, onTopBtn } from './js/scroll';
 import { addDarkClassToHTML } from './js/theme-btn';
+
 getPopularFilms();
 import { modalMurkup } from './js/modal';
 import './js/team-modal';
@@ -13,4 +14,3 @@ import './js/team-modal';
 if (refs.formEl) {
   refs.formEl.addEventListener('submit', e => getFilmsFromInput(e));
 }
-
--- src/js/theme-btn.js
+++ src/js/theme-btn.js
@@ -1,22 +1,3 @@
-// const themeBtn = document.querySelector('.toggleThemeBtn');
-// const mainTheme = document.querySelector('#main-home');
-// const currentTheme = localStorage.getItem('theme');
-
-// if (currentTheme === 'dark') {
-//   mainTheme.classList.add('toggle-theme');
-// }
-
-// themeBtn.addEventListener('click', onThemeBtn);
-
-// function onThemeBtn() {
-//   mainTheme.classList.toggle('toggle-theme');
-//   let theme = 'white';
-//   if (mainTheme.classList.contains('toggle-theme')) {
-//     theme = 'dark';
-//   }
-//   localStorage.setItem('theme', theme);
-// }
-
 document.querySelector('.toggleThemeBtn').addEventListener('click', event => {
   event.preventDefault();
   if (localStorage.getItem('theme') === 'dark') {
