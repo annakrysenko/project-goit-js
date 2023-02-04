7d94ac190a7f8951eb828bd21953089b56195d4e
Serhii Soloviov <115696868+Serhii-Soloviov@users.noreply.github.com>
2023-02-04 13:47:42 +0200

    ff

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -8,7 +8,7 @@ function createMovieIframe(e) {
     const movieId = e.target.parentNode.parentNode.dataset.id;
     getMovieTrailerByID(movieId)
       .then(data => {
-        const id = data.results[1].key;
+        const id = data.results[0].key;
         console.log(id);
         const instance = basicLightbox.create(
           `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
@@ -29,11 +29,26 @@ function createMovieIframe(e) {
         }
       })
       .catch(() => {
-        const instance = basicLightbox.create(`
+        const instance = basicLightbox.create(
+          `
     <iframe width="560" height="315" src='http://www.youtube.com/embed/zwBpUdZ0lrQ' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      `);
+      `,
+          {
+            onShow: instance => {
+              document.addEventListener('keydown', closeMovieTrailer);
+            },
+            onClose: instance => {
+              document.removeEventListener('keydown', closeMovieTrailer);
+            },
+          }
+        );
 
         instance.show();
+        function closeMovieTrailer(event) {
+          if (event.key === 'Escape') {
+            instance.close();
+          }
+        }
       });
   } else {
     return;
