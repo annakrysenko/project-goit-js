bfc3c7445ac321b33e338b9836b5d837b2b72898
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-02-05 19:51:18 +0200

    styles

--- src/js/modal/modal-markup.js
+++ src/js/modal/modal-markup.js
@@ -6,17 +6,6 @@ export function modalMarkup(movie) {
   const popularity = movie.popularity.toFixed(1);
   const movieTitle = movie.title.toUpperCase()
   return `
-<<<<<<< Updated upstream
-  <img class = 'modal__img' src="${BASE_URL}${movie.poster_path}" alt="movie-poster" /></a>
-  <h2 class = 'modal__title'>${movie.title}</h2>
-  <p>Vote / Votes ${movie.vote_average}/${movie.vote_count}</p>
-  <p>Popularity ${movie.popularity}</p>
-  <p>Original Title ${movie.original_title} </p>
-  // <p>Genre ${genresName}</p>
-  <h3>About </h3>
-  <p>${movie.overview}</p>
-  <button data-id="${movie.id}" class="trailers" type="button" data-modal-open>Watch trailer</button>
-=======
   <img class = 'modal-container__img' src="${BASE_URL}${movie.poster_path}" alt="movie-poster" /></a>
   <h2 class = 'modal-container__title'>${movieTitle}</h2>
   <div class = 'modal-container__info'>
@@ -28,15 +17,17 @@ export function modalMarkup(movie) {
   </div>
   <div class = 'modal-container__info--value'>
   <p class = 'modal-container__info--valuetext'><span class = 'avarage-effects'>${avarage}</span> / <span class = 'count-effect'>${movie.vote_count}</span></p>
- <p class = 'modal-container__info--valuetext'>${popularity}</p>
+  <p class = 'modal-container__info--valuetext'>${popularity}</p>
   <p class = 'modal-container__info--valuetext'>${movieTitle}</p>
   <p class = 'modal-container__info--valuetext'>${genresName}</p>
   </div>
   </div>
   <h3 class = 'modal-container__second-title'>ABOUT</h3>
-  <p modal-container__description>${movie.overview}</p>
->>>>>>> Stashed changes
+  <p class = 'modal-container__description'>${movie.overview}</p>
   <button class="watched" type="button">add to Watched</button>
-  <button class="queue" type="button">add to queue</button>
+  <button" class="queue" type="button">add to queue</button>
+  <button data-id="${movie.id}" class="trailers" type="button" data-modal-open>Watch trailer
+  <img src="./images/trailer.svg" width="30" height="30" />
+  </button>
   `
 }
