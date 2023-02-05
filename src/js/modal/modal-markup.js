ba46034015aab70968b0f7b2ca59b7fbd39bc10c
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-02-05 19:11:00 +0200

    styles

--- src/js/modal/modal-markup.js
+++ src/js/modal/modal-markup.js
@@ -2,7 +2,11 @@ const BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 export function modalMarkup(movie) {
   const genresName = movie.genresArr.join(', ');
+  const avarage = movie.vote_average.toFixed(1);
+  const popularity = movie.popularity.toFixed(1);
+  const movieTitle = movie.title.toUpperCase()
   return `
+<<<<<<< Updated upstream
   <img class = 'modal__img' src="${BASE_URL}${movie.poster_path}" alt="movie-poster" /></a>
   <h2 class = 'modal__title'>${movie.title}</h2>
   <p>Vote / Votes ${movie.vote_average}/${movie.vote_count}</p>
@@ -12,8 +16,27 @@ export function modalMarkup(movie) {
   <h3>About </h3>
   <p>${movie.overview}</p>
   <button data-id="${movie.id}" class="trailers" type="button" data-modal-open>Watch trailer</button>
+=======
+  <img class = 'modal-container__img' src="${BASE_URL}${movie.poster_path}" alt="movie-poster" /></a>
+  <h2 class = 'modal-container__title'>${movieTitle}</h2>
+  <div class = 'modal-container__info'>
+  <div class = 'modal-container__info--key'> 
+  <p class = 'modal-container__info--keytext'>Vote / Votes</p>
+  <p class = 'modal-container__info--keytext'>Popularity</p>
+  <p  class = 'modal-container__info--keytext'>Original Title</p>
+  <p  class = 'modal-container__info--keytext'>Genre</p>
+  </div>
+  <div class = 'modal-container__info--value'>
+  <p class = 'modal-container__info--valuetext'><span class = 'avarage-effects'>${avarage}</span> / <span class = 'count-effect'>${movie.vote_count}</span></p>
+ <p class = 'modal-container__info--valuetext'>${popularity}</p>
+  <p class = 'modal-container__info--valuetext'>${movieTitle}</p>
+  <p class = 'modal-container__info--valuetext'>${genresName}</p>
+  </div>
+  </div>
+  <h3 class = 'modal-container__second-title'>ABOUT</h3>
+  <p modal-container__description>${movie.overview}</p>
+>>>>>>> Stashed changes
   <button class="watched" type="button">add to Watched</button>
   <button class="queue" type="button">add to queue</button>
-
-`;
+  `
 }
--- src/js/trailer.js
+++ src/js/trailer.js
@@ -13,8 +13,61 @@ refs.modalEl.addEventListener('click', createMovieIframe);
 //
 window.addEventListener('keydown', closeMovieTrailer);
 
+<<<<<<< Updated upstream
 async function createMovieIframe(e) {
   if (e.target.className !== 'trailers') {
+=======
+function createMovieIframe(e) {
+  if (e.target.className === 'modal__trailers-btn') {
+    const movieId = e.target.dataset.id;
+    console.log(movieId);
+    getMovieTrailerByID(movieId)
+      .then(data => {
+        console.log();
+        const id = data.results[0].key;
+        console.log(id);
+        const instance = basicLightbox.create(
+          `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+          {
+            onShow: instance => {
+              document.addEventListener('keydown', closeMovieTrailer);
+            },
+            onClose: instance => {
+              document.removeEventListener('keydown', closeMovieTrailer);
+            },
+          }
+        );
+        instance.show();
+        function closeMovieTrailer(event) {
+          if (event.key === 'Escape') {
+            instance.close();
+          }
+        }
+      })
+      .catch(() => {
+        const instance = basicLightbox.create(
+          `
+    <iframe width="560" height="315" src='http://www.youtube.com/embed/zwBpUdZ0lrQ' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      `,
+          {
+            onShow: instance => {
+              document.addEventListener('keydown', closeMovieTrailer);
+            },
+            onClose: instance => {
+              document.removeEventListener('keydown', closeMovieTrailer);
+            },
+          }
+        );
+
+        instance.show();
+        function closeMovieTrailer(event) {
+          if (event.key === 'Escape') {
+            instance.close();
+          }
+        }
+      });
+  } else {
+>>>>>>> Stashed changes
     return;
   }
   refs.trailerBackdrop.classList.remove('is-hidden');
